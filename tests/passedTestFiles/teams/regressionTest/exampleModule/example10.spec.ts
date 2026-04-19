9a0015a250e02bed24c3c7f97cfe1f11c20bb61e
Linkin143 <kumarprince143p143@gmail.com>
2026-04-20 04:22:15 +0530

    some files deleted

--- tests/passedTestFiles/teams/regressionTest/exampleModule/example10.spec.ts
+++
@@ -1,7 +0,0 @@
-// example9.spec.ts
-import { expect, test } from '@playwright/test';
-
-test('homepage test', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example/);
-});
